894356d67f6f9d7529d8077c0e71deff0eb69539
harsh panchal <2101031000120@silveroakuni.ac.in>
2024-09-07 22:15:34 +0530

    works fine for sign up , log in and home page

--- src/App.js
+++ src/App.js
@@ -4,18 +4,16 @@ import SignUpForm from './components/Auth/Signup.js';
 import HomePage from './pages/Home.js';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
-    return (
-        <div>
-          {/* <Route path="/" element={<HomePage />} />  
-        <Route path="/login" element={<LoginForm />} />  
-        <Route path="/signup" element={<SignUpForm />} />   */}
-
-        {/* <LoginForm/>
-        <SignUpForm/> */}
-        <HomePage/>
-            </div>
-    );
-}
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/signup" element={<SignUpForm />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
